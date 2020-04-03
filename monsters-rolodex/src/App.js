b1934cff3afb785f33bfc5f91dbd7ab03143fecc
Edmilson <edmylsondih@gmail.com>
2020-04-03 02:36:49 -0300

    Deploy

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { CardList } from './Components/CardList/CardList';
+import { SearchBox } from './Components/SearchBox/SearchBox';
+import './App.css';
 
 class App extends React.Component {
   constructor(){
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -16,10 +19,19 @@ class App extends React.Component {
     .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = e => {
+    this.setState( {searchField: e.target.value} )
+  }
+
   render(){
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
     return (
       <div className="App">
-        <CardList monsters={ this.state.monsters } />
+        <h1>Monster Rolodex</h1>
+        <SearchBox placeholder='Search a monster' handleChange={ this.handleChange } />
+        <CardList monsters={ filteredMonsters } />
       </div>
     );
   }
