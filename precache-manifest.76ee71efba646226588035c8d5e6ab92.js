9a99686c8c10416746823b1184df6972be39082b
Edmilson <edmylsondih@gmail.com>
2020-04-03 02:35:31 -0300

    Updates

---
+++ precache-manifest.76ee71efba646226588035c8d5e6ab92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e354d51ac0d638f8c8c48c807a3f76",
+    "url": "/MonsterRolodex/index.html"
+  },
+  {
+    "revision": "5eeba9382e3cb2d6ba27",
+    "url": "/MonsterRolodex/static/css/main.75c01f0b.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/MonsterRolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterRolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eeba9382e3cb2d6ba27",
+    "url": "/MonsterRolodex/static/js/main.bdf53e99.chunk.js"
+  },
+  {
+    "revision": "52668989ac04e5dc8a9f",
+    "url": "/MonsterRolodex/static/js/runtime-main.eb7a3110.js"
+  }
+]);
